3773f871ae2577c7ebd1d9ad9206d6a09f52b509
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-24 15:10:33 +0100

    feat: printed  on screen dice game result

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -11,18 +11,27 @@ alert("Wanna play dice?");
 const playerResult = Math.floor(Math.random() * 6) + 1;
 const machineResult = Math.floor(Math.random() * 6) + 1;
 
+//define let variable for show result on screen
+
+let shownResult = "";
+
 //confront the 2 results 
 
 if  (playerResult > machineResult)  {
 
     console.log("you won!!!");
+    shownResult = ("you won!!!");
 
 } else if (playerResult < machineResult) {
 
     console.log("you lost!!!");
+    shownResult = ("you lost!!!");
 
 } else {
 
     console.log("draw!!!");
+    shownResult = ("draw!!!");
 
 }
+
+document.querySelector("h1").innerHTML = `${shownResult}`;
